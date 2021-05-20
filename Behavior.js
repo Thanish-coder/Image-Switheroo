8a6df1c4243b0ea1d9dfcbb2d7637971c3489e20
Thanish-coder <83280895+Thanish-coder@users.noreply.github.com>
2021-05-20 22:18:56 +0530

    Create Behavior.js

---
+++ Behavior.js
@@ -0,0 +1,7 @@
+function off() {
+  document.getElementById("imagen").src="https://www.lamps-on-line.com/media/catalog/product/cache/1/thumbnail/1200x/9df78eab33525d08d6e5fb8d27136e95/b/u/bulb-clear-es_10.jpg"
+}
+function on() {
+  document.getElementById("imagen").src="https://www.texturesbackgrounds.com/files/glowing-light-bulb-background.jpg"
+ document.getElementById("Body").style.color=Yellow
+}
